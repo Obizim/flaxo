8991c472fc5323a4a077104b65ded6c08a5869d9
Zim <obizimuzo@gmail.com>
2021-11-03 00:30:18 -0700

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+     <p>Hello, testing the mic before my initial git commit ...... hahahahahaha</p>
+    </div>
+  );
+}
+
+export default App;
